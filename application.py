0d5b0e0cf1b28e5917f72245d3b61b5aad8b423d
Mayura21 <mayurasm2000@gmail.com>
2025-07-02 20:02:57 +0530

    Files

--- application.py
+++ application.py
@@ -7,10 +7,36 @@ from sklearn.preprocessing import StandardScaler
 application = Flask(__name__)
 app = application
 
+# Import regression and standard scalerpickle
+regressor = pickle.load(open('models/regression.pkl', 'rb'))
+scaler = pickle.load(open('models/scaler.pkl', 'rb'))
+
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello World!</h1>"
+def index():
+    return render_template('index.html', debug=True)
+
+
+@app.route('/predict_data', methods=['GET', 'POST'])
+def predict_data():
+    if request.method == 'POST':
+        temp = float(request.form['temperature'])
+        rh = float(request.form['humidity'])
+        ws = float(request.form['wind_speed'])
+        rain = float(request.form['rain'])
+        ffmc = float(request.form['ffmc'])
+        dmc = float(request.form['dmc'])
+        dc = float(request.form['dc'])
+        isi = float(request.form['isi'])
+        bui = float(request.form['bui'])
+        region = float(request.form['region'])
+        new_data_scaled = scaler.transform([[temp, rh, ws, rain, ffmc, dmc, isi, region]])
+        result = regressor.predict(new_data_scaled)
+
+        return render_template('home.html', results=result[0])
+    else:
+        return render_template('home.html')
+    return
 
 
 if __name__ == "__main__":
